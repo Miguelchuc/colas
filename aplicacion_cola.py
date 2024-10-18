bd79113118a2d069c39cd4da2e5bd12b56def23d
TedeLimonn <Limonmike14@gmail.com>
2024-10-17 18:28:15 -0600

    aplicacion_cola.py

--- aplicacion_cola.py
+++ aplicacion_cola.py
@@ -7,15 +7,24 @@ class Cola:
     def agregar(self, valor):
         self.elementos.append(valor)
     
-    def buscar(self, valor):
-        return valor in self.elementos
+    def atender(self, valor):
+        if valor in self.elementos:
+            self.elementos.remove(valor)
+            return True
+        return False
+    
+    def __str__(self):
+        return f"[ {' , '.join(self.elementos)} ]"
 
-def mostrar_menu():
+def mostrar_menu_principal():
     print("1. Elegir Cola A")
     print("2. Elegir Cola B")
-    print("3. Ingresar cliente")
-    print("4. Atender cliente")
-    print("5. Salir")
+    print("3. Salir")
+
+def mostrar_menu_colas():
+    print("1. Ingresar cliente")
+    print("2. Atender cliente")
+    print("3. Volver al menú principal")
 
 def main():
     cola_a = Cola()
@@ -23,34 +32,41 @@ def main():
     cola_elegida = None
     
     while True:
-        mostrar_menu()
-        opcion = input("\nPara iniciar elija primero la cola para realizar el ingreso o atencion del cliente\n ")
+        mostrar_menu_principal()
+        opcion = input("\nSeleccione una opción: ")
 
         if opcion == '1':
             cola_elegida = cola_a
-            print("Cola 1 elegida.")
+            print("\nCola A elegida.")
         elif opcion == '2':
             cola_elegida = cola_b
-            print("Cola 2 elegida.")
+            print("\nCola B elegida.")
         elif opcion == '3':
-            if cola_elegida is None:
-                print("Primero debe elegir una cola.")
-            else:
-                valor = input("Ingrese el numero de atencion del cliente : ")
-                cola_elegida.agregar(valor)
-                print(f"Valor '{valor}' agregado a la cola {cola_elegida}.")
-        elif opcion == '4':
-            if cola_elegida is None:
-                print("Primero debe elegir una cola.")
-            else:
-                valor_buscar = input("Ingrese el numero de atencion del cliente: ")
-                if cola_elegida.buscar(valor_buscar):
-                    print(f"el cliente con el numero '{valor_buscar}' fue atendido.")
-        elif opcion == '5':
             print("Saliendo del programa.")
             break
         else:
             print("Opción no válida. Intente de nuevo.")
+            continue
+        
+        while True:
+            mostrar_menu_colas()
+            opcion_colas = input("\nSeleccione una opción para la cola elegida: ")
+
+            if opcion_colas == '1':
+                valor = input("Ingrese el numero de atencion del cliente: ")
+                cola_elegida.agregar(valor)
+                print(f"Valor '{valor}' agregado a la cola: {cola_elegida}.")
+            elif opcion_colas == '2':
+                valor_buscar = input("Ingrese el numero de atencion del cliente a atender: ")
+                if cola_elegida.atender(valor_buscar):
+                    print(f"\nel cliente con el numero '{valor_buscar}' fue atendido.")
+                    print(f"Estado actual de la cola: {cola_elegida}.")
+                else:
+                    print(f"\nel cliente con el numero '{valor_buscar}' no se encuentra en la cola.")
+            elif opcion_colas == '3':
+                break
+            else:
+                print("Opción no válida. Intente de nuevo.")
 
 if __name__ == "__main__":
     main()
